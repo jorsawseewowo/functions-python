7ed0ea603b606ac228277f0cf9d3b450cd0947e7
jorsawseewowo <joachimandres.seydel@gmail.com>
2026-01-31 17:44:49 +0700

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+terms = 15
+for i in range(terms):
+    print(fibonacci(i))
